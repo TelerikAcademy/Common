c5af1b50952268e651e179d825f6eacef33aad10
Doncho Minkov <donchominkov@gmail.com>
2016-03-28 12:13:20 +0300

    Added links for follow

--- revealjs-theme/js/main.js
+++ revealjs-theme/js/main.js
@@ -5,13 +5,13 @@ $(function() {
     .append('<div class="social-widget left desk">' +
       '<strong>Follow us</strong>' +
       '<br/>' +
-      '<a href="#"><img title="Our website"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/telerik_academy.png" /></a>' +
-      '<a href="#"><img title="Follow us"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/facebook.png" /></a>' +
-      '<a href="#"><img title="Subscribe"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/youtube.png" /></a>' +
-      '<a href="#"><img title="Add us to circles"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/google_plus.png" /></a>' +
-      '<a href="#"><img title="Follow"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/linkedin.png" /></a>' +
-      '<a href="#"><img  title="Follow" src="https://rawgit.com/TelerikAcademy/Common/master/icons/twitter.png" /></a>' +
-      '<a href="#"><img title="Subscribe"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/rss.png" /></a>' +
+      '<a href="http://telerikacademy.com"><img title="Our website"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/telerik_academy.png" /></a>' +
+      '<a href="https://www.facebook.com/TelerikAcademy"><img title="Follow us"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/facebook.png" /></a>' +
+      '<a href="https://www.youtube.com/user/telerikacademy"><img title="Subscribe"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/youtube.png" /></a>' +
+      '<a href="https://plus.google.com/u/0/+telerikacademy/"><img title="Add us to circles"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/google_plus.png" /></a>' +
+      '<a href="https://www.linkedin.com/groups/3161033"><img title="Follow"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/linkedin.png" /></a>' +
+      '<a href="https://twitter.com/TelerikAcademy"><img  title="Follow" src="https://rawgit.com/TelerikAcademy/Common/master/icons/twitter.png" /></a>' +
+      '<a href="http://feeds.feedburner.com/TelerikAcademy"><img title="Subscribe"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/rss.png" /></a>' +
       '</div>');
 
   // $('#presentation')
@@ -24,13 +24,13 @@ $(function() {
     .append('  <div class="social-widget left mobile">' +
       '<strong>Follow us</strong>' +
       '<div class="buttons">' +
-      '  <a href="#"><img title="Our website" src="https://rawgit.com/TelerikAcademy/Common/master/icons/telerik_academy.png" /></a>' +
-      '  <a href="#"><img title="Follow us" src="https://rawgit.com/TelerikAcademy/Common/master/icons/facebook.png" /></a>' +
-      '  <a href="#"><img title="Subscribe" src="https://rawgit.com/TelerikAcademy/Common/master/icons/youtube.png" /></a>' +
-      '  <a href="#"><img title="Add us to circles" src="https://rawgit.com/TelerikAcademy/Common/master/icons/google_plus.png" /></a>' +
-      '  <a href="#"><img title="Follow" src="https://rawgit.com/TelerikAcademy/Common/master/icons/linkedin.png" /></a>' +
-      '  <a href="#"><img title="Follow" src="https://rawgit.com/TelerikAcademy/Common/master/icons/twitter.png" /></a>' +
-      '  <a href="#"><img title="Subscribe" src="https://rawgit.com/TelerikAcademy/Common/master/icons/rss.png" /></a>' +
+      '<a href="http://telerikacademy.com"><img title="Our website"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/telerik_academy.png" /></a>' +
+      '<a href="https://www.facebook.com/TelerikAcademy"><img title="Follow us"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/facebook.png" /></a>' +
+      '<a href="https://www.youtube.com/user/telerikacademy"><img title="Subscribe"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/youtube.png" /></a>' +
+      '<a href="https://plus.google.com/u/0/+telerikacademy/"><img title="Add us to circles"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/google_plus.png" /></a>' +
+      '<a href="https://www.linkedin.com/groups/3161033"><img title="Follow"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/linkedin.png" /></a>' +
+      '<a href="https://twitter.com/TelerikAcademy"><img  title="Follow" src="https://rawgit.com/TelerikAcademy/Common/master/icons/twitter.png" /></a>' +
+      '<a href="http://feeds.feedburner.com/TelerikAcademy"><img title="Subscribe"  src="https://rawgit.com/TelerikAcademy/Common/master/icons/rss.png" /></a>' +
       '</div>' +
       '</div>');
 
